006c90a55ccc9350fa746c7b646ab8769475ccdb
jin <jin@jinui-MacBookPro.local>
2025-05-06 09:28:34 +0900

    add : point

---
+++ src/main/java/com/mall/application/dto/UserPointDto.java
@@ -0,0 +1,12 @@
+package com.mall.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPointDto {
+    private Long point;
+}
--- src/main/java/com/mall/controller/UserPointController.java
+++ src/main/java/com/mall/controller/UserPointController.java
@@ -2,13 +2,13 @@ package com.mall.controller;
 
 import com.mall.annotation.CurrentUser;
 import com.mall.application.dto.UserInfo;
+import com.mall.application.dto.UserPointDto;
+import com.mall.code.PointType;
 import com.mall.common.CommonResponse;
 import com.mall.service.UserPointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/point")
@@ -19,5 +19,11 @@ public class UserPointController {
     @GetMapping("/total")
     public ResponseEntity<CommonResponse<Long>> getTotalPoint(@CurrentUser UserInfo userInfo) {
         return CommonResponse.ok(userPointService.totalPoint(userInfo.getId()));
+    }
+
+    @PostMapping()
+    public ResponseEntity<CommonResponse<Boolean>> create(@CurrentUser UserInfo userInfo, @RequestBody UserPointDto dto) {
+        userPointService.updatePoint(userInfo.getId(), dto.getPoint(), PointType.EARN);
+        return CommonResponse.ok(Boolean.TRUE);
     }
 }
